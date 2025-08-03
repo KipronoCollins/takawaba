1fd1d4661653704f00ee0135dae09d40dec6a283
kipronocollins <kipkirui42@gmail.com>
2025-08-03 12:42:29 +0300

    first push

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    //
+    public function home()
+    {
+        return view('pages.home');
+    }
+
+    public function about()
+    {
+        return view('pages.about');
+    }
+
+    public function ceoMessage()
+    {
+        return view('pages.ceoMessage');
+    }
+
+    public function policy()
+    {
+        return view('pages.policy');
+    }
+    
+    public function board()
+    {
+        return view('pages.board');
+    }
+
+    public function management()
+    {
+        return view('pages.managementTeam');
+    }
+
+    public function contact()
+    {
+        return view('pages.contact');
+    }
+    public function lab()
+    {
+        return view('pages.lab');
+    }
+    public function sources()
+    {
+        return view('pages.sources');
+    }
+    public function serviceArea()
+    {
+        return view('pages.serviceArea');
+    }
+    public function sewer()
+    {
+        return view('pages.sewer');
+    }
+    public function waterServices()
+    {
+        return view('pages.waterServices');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+
+
+Route::get('/', [MainController::class, 'home']);
+Route::get('about', [MainController::class, 'about']);
+Route::get('ceoMessage', [MainController::class, 'ceoMessage']);
+Route::get('policy', [MainController::class, 'policy']);
+Route::get('board', [MainController::class, 'board']);
+Route::get('managementTeam', [MainController::class, 'management']);
+Route::get('contact', [MainController::class, 'contact']);
+Route::get('lab', [MainController::class, 'lab']);
+Route::get('sources', [MainController::class, 'sources']);
+Route::get('serviceArea', [MainController::class, 'serviceArea']);
+Route::get('sewer', [MainController::class, 'sewer']);
+Route::get('waterServices', [MainController::class, 'waterServices']);
+
+
