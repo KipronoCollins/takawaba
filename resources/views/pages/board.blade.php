02b9d64bec84daeec39d5ff9120e5530bb8e5010
kipronocollins <kipkirui42@gmail.com>
2025-08-03 23:14:14 +0300

    sort content

--- resources/views/pages/board.blade.php
+++ resources/views/pages/board.blade.php
@@ -16,11 +16,11 @@
 
           <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
             <div class="team-member d-flex align-items-start">
-              <div class="pic"><img src="{{ asset('assets/img/doctors/doctors-1.jpg') }}" class="img-fluid" alt=""></div>
+              <div class="pic"><img src="{{ asset('assets/img/doctors/b1.jpg') }}" class="img-fluid" alt=""></div>
               <div class="member-info">
-                <h4>Eng. Walter Mwangi</h4>
-                <span>Board Chairperson</span>
-                <p>Provides strategic oversight to ensure the company delivers sustainable water solutions to the community.</p>
+                <h4>Adan Mohamed Issack</h4>
+                <span>Board Member</span>
+                {{-- <p>Provides strategic oversight to ensure the company delivers sustainable water solutions to the community.</p> --}}
                 <div class="social">
                   <a href=""><i class="bi bi-twitter-x"></i></a>
                   <a href=""><i class="bi bi-facebook"></i></a>
@@ -33,11 +33,11 @@
 
           <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
             <div class="team-member d-flex align-items-start">
-              <div class="pic"><img src="{{ asset('assets/img/doctors/doctors-2.jpg') }}" class="img-fluid" alt=""></div>
+              <div class="pic"><img src="{{ asset('assets/img/doctors/b2.jpg') }}" class="img-fluid" alt=""></div>
               <div class="member-info">
-                <h4>Sarah Juma</h4>
-                <span>Finance Director</span>
-                <p>Oversees financial planning, budgeting, and ensures funds are used effectively to support operations and expansion.</p>
+                <h4>Mohamed Abdirahman</h4>
+                <span>Board Member</span>
+                {{-- <p>Oversees financial planning, budgeting, and ensures funds are used effectively to support operations and expansion.</p> --}}
                 <div class="social">
                   <a href=""><i class="bi bi-twitter-x"></i></a>
                   <a href=""><i class="bi bi-facebook"></i></a>
@@ -50,11 +50,11 @@
 
           <div class="col-lg-6" data-aos="fade-up" data-aos-delay="300">
             <div class="team-member d-flex align-items-start">
-              <div class="pic"><img src="{{ asset('assets/img/doctors/doctors-3.jpg') }}" class="img-fluid" alt=""></div>
+              <div class="pic"><img src="{{ asset('assets/img/doctors/b3.jpg') }}" class="img-fluid" alt=""></div>
               <div class="member-info">
-                <h4>William Otieno</h4>
-                <span>Technical Director</span>
-                <p>Leads infrastructure development, water treatment technologies, and pipeline expansion initiatives.</p>
+                <h4>Habiba Adan Mohamed</h4>
+                <span>Board Member</span>
+                {{-- <p>Leads infrastructure development, water treatment technologies, and pipeline expansion initiatives.</p> --}}
                 <div class="social">
                   <a href=""><i class="bi bi-twitter-x"></i></a>
                   <a href=""><i class="bi bi-facebook"></i></a>
@@ -67,11 +67,11 @@
 
           <div class="col-lg-6" data-aos="fade-up" data-aos-delay="400">
             <div class="team-member d-flex align-items-start">
-              <div class="pic"><img src="{{ asset('assets/img/doctors/doctors-4.jpg') }}" class="img-fluid" alt=""></div>
+              <div class="pic"><img src="{{ asset('assets/img/doctors/b4.jpg') }}" class="img-fluid" alt=""></div>
               <div class="member-info">
-                <h4>Amanda Njeri</h4>
-                <span>Customer & Community Director</span>
-                <p>Focuses on community engagement, customer service excellence, and access to safe water in underserved areas.</p>
+                <h4>Zeinab Mohamed Adan</h4>
+                <span>Board Member</span>
+                {{-- <p>Focuses on community engagement, customer service excellence, and access to safe water in underserved areas.</p> --}}
                 <div class="social">
                   <a href=""><i class="bi bi-twitter-x"></i></a>
                   <a href=""><i class="bi bi-facebook"></i></a>
